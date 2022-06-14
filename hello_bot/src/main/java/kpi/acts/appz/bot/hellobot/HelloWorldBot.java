4c7cc93329aba78eeaec6a8eed51653c68d4f888
co1umb <66077930+co1umb@users.noreply.github.com>
2022-06-14 15:53:44 +0300

    7
    7

--- hello_bot/src/main/java/kpi/acts/appz/bot/hellobot/HelloWorldBot.java
+++ hello_bot/src/main/java/kpi/acts/appz/bot/hellobot/HelloWorldBot.java
@@ -22,6 +22,6 @@ public final class HelloWorldBot extends Bot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        sendTextMessage(update.getMessage(), "Hell");
+        sendTextMessage(update.getMessage(), "Hello World!");
     }
 }
